deb524d9ce8cd36a395dc393d4598346cef78c0e
Henry <henry_bamby@yahoo.es>
2022-01-08 05:08:55 +0100

    primer commit

---
+++ src/index.js
@@ -0,0 +1,7 @@
+
+import {saludar} from './js/componentes';
+import './styles.css';
+
+const nombre = "Henry Alvarado";
+
+saludar(nombre); 
---
+++ src/js/componentes.js
@@ -0,0 +1,25 @@
+
+import '../css/components.css';
+import webpacklogo from '../assets/imgs/webpack-logo.png';
+
+
+export const saludar = (nombre) =>{
+    console.log('Creando Etiqueta h1');
+
+
+    const h1 = document.createElement('h1');
+      h1.innerText= `Hola ${nombre} esta es tu primera web`;
+      
+
+      document.body.append(h1);
+
+
+      //img
+       
+      console.log(webpacklogo);
+      const img = document.createElement('img');
+      img.src=webpacklogo;
+      document.body.append(img);
+      
+    
+}
